ddb81c0d79e4da70136f66d75c05f5a400ebd68f
James Harris <james.harris@icecave.com.au>
2013-02-18 18:55:59 +1000

    Added support for Sublime Text as EDITOR/VISUAL.

--- lib/editor.bash
+++ lib/editor.bash
@@ -1,7 +1,21 @@
+# Sublime Text 3 ...
+if [ -d "/Applications/Sublime\ Text.app/Contents/SharedSupport/bin" ]; then
+	PATH="$PATH:/Applications/Sublime\ Text.app/Contents/SharedSupport/bin"
+# Sublime Text 2
+elif [ -d "/Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin" ]; then
+	PATH="$PATH:/Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin"
+fi
+
+SUBLIME=$(which subl 2>/dev/null)
 TEXTMATE=$(which mate 2>/dev/null)
 
-if [[ $TEXTMATE ]]; then
+if [[ $SUBLIME ]]; then
+	VISUAL="${SUBLIME} -w"
+elif [[ $TEXTMATE ]]; then
     VISUAL="${TEXTMATE} -w"
-    EDITOR="${TEXTMATE} -w"
-    git config --global core.editor "$VISUAL"
+else
+	VISUAL=vim
 fi
+
+EDITOR="$VISUAL"
+git config --global core.editor "$VISUAL"
