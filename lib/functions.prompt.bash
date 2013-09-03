7bf977641d418a8ab1ae3e4872abaa59f1f07af3
James Harris <james.harris@icecave.com.au>
2013-09-03 13:39:53 +1000

    Added title function for overriding window title.

--- lib/functions.prompt.bash
+++ lib/functions.prompt.bash
@@ -1,41 +1,45 @@
 PROMPT_COMMAND=build-prompt
 DEFAULT_USERNAME=james
 
+function title {
+    WINDOW_TITLE="$1"
+}
+
 function build-prompt {
     ESCAPE_COLOR_CODES=true
 
     local auth=''
-    if [[ $USER != $DEFAULT_USERNAME ]]; then
+    if [[ "$USER" != "$DEFAULT_USERNAME" ]]; then
         auth=$(color-yellow $USER)
     fi
 
-    if [ $SSH_TTY ]; then
+    if [ "$SSH_TTY" ]; then
         auth="${auth}$(color-yellow "@\h")"
     fi
 
-    if [[ $auth != "" ]]; then
+    if [[ "$auth" != "" ]]; then
         auth=" ${auth}"
     fi
 
     local location=$(color-blue "\w")
     local title=""
 
-    if [ $REAL_GIT ]; then
+    if [ "$REAL_GIT" ]; then
         local repo=$(git-repo)
 
-        if [[ $repo != "" ]]; then
+        if [[ "$repo" != "" ]]; then
             local branch=$(git-branch)
             title="$repo"
 
-            if [[ $branch == "develop" ]]; then
+            if [[ "$branch" == "develop" ]]; then
                 branch=$(color-white develop)
-            elif [[ $branch == "master" ]]; then
+            elif [[ "$branch" == "master" ]]; then
                 branch=$(color-red master)
             else
                 local rev=$(git-rev)
-                if [[ $rev == "" ]]; then
+                if [[ "$rev" == "" ]]; then
                     branch=$(color-yellow "<empty>")
-                elif [[ $branch == "" ]]; then
+                elif [[ "$branch" == "" ]]; then
                     branch=$(color-red "<${rev}>")
                 else
                     branch=$(color-green $branch)
@@ -45,10 +49,14 @@ function build-prompt {
             location="$(color-magenta $repo) ${branch}"
 
             local path=$(git-path)
-            if [[ $path != "" ]]; then
+            if [[ "$path" != "" ]]; then
                 location="$location $(color-blue "$path")"
             fi
         fi
+    fi
+
+    if [ "$WINDOW_TITLE" ]; then
+        title="$WINDOW_TITLE"
     fi
 
     PS1="\[\033]0;${title}\007\]$(color-dark-grey "\A")$auth $location$(color-dark-grey :) $(color-reset)"
