f4296960e119b08dfb87fc753cb48d2712f50a33
James Harris <james.harris@icecave.com.au>
2017-11-15 20:42:32 +1000

    Install grit via homebrew.

--- install.d/99.dev.grit.bash
+++
@@ -1,7 +0,0 @@
-if [ ! -e "$HOME/bin/grit" ]; then
-    pushd "$HOME/bin" > /dev/null
-    curl -L "https://github.com/jmalloc/grit/releases/download/0.6.4/grit-darwin-amd64.tar.gz" | tar -x
-    popd > /dev/null
-fi
-
-$HOME/bin/grit self-update
