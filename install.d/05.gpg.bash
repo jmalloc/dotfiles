aac2938667f4201b1788f4242556c58f003ab9f3
James Harris <james.harris@icecave.com.au>
2017-11-16 08:21:21 +1000

    Add key ID to Git config.

--- install.d/05.gpg.bash
+++ install.d/05.gpg.bash
@@ -5,6 +5,7 @@ if [ ! -e "$HOME/.gnupg/privkey.pem" ]; then
     op get document --account=my "GPG Private Key" > "$HOME/.gnupg/privkey.pem"
     chmod 600 "$HOME/.gnupg/privkey.pem"
     gpg --import "$HOME/.gnupg/privkey.pem"
+    git config --global user.signingkey "$(op get item --account=my "GPG Private Key" | jq --raw-output '.details.sections[].fields[]? | select(.t == "ID") | .v')"
 fi
 
 if [ ! -e "$HOME/.gnupg/pubkey.pem" ]; then
