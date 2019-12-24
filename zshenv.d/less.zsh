863664ca3390c56095836e3d6f49b8e6b8606fb6
James Harris <mailjamesharris@gmail.com>
2019-12-24 12:27:29 +1000

    Configure behavior of `ls` and `less`.

---
+++ zshenv.d/less.zsh
@@ -0,0 +1,1 @@
+export LESS="--hilite-search --hilite-unread --ignore-case --LONG-PROMPT --RAW-CONTROL-CHARS --chop-long-lines  --window=-4"
--- zshrc.d/aliases.zsh
+++ zshrc.d/aliases.zsh
@@ -3,3 +3,6 @@ alias e='code .'
 alias g='git'
 alias h='hive'
 alias m='make'
+
+alias ls='ls -lhG'
+alias la='ls -lhGa'
