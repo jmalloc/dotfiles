2aa2d3d6ec46991f81c839709a0c77b28eff4b77
James Harris <mailjamesharris@gmail.com>
2020-01-30 12:51:17 +1000

    Render prompt segments using yellow-over-red.

--- zshrc.d/prompt.zsh
+++ zshrc.d/prompt.zsh
@@ -12,11 +12,11 @@ function prompt_docker() {
         return
     fi
 
-    p10k segment -b 1 -f 7 -t "$host"
+    p10k segment -b 1 -f 11 -t "$host"
 }
 
 function prompt_gitwip() {
     if git log -n 1 --pretty=%B 2>/dev/null | grep WIP > /dev/null; then
-        p10k segment -b 1 -f 7 -t "WORK IN PROGRESS"
+        p10k segment -b 1 -f 11 -t "WORK IN PROGRESS"
     fi
 }
