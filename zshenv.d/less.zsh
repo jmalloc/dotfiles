f4e39750897a5d8d924b832dde87b171a6adb003
James Harris <mailjamesharris@gmail.com>
2020-02-29 08:06:37 +1000

    Prevent less from paging if there's only one screen.
    This required installing a more recent less version.
    See https://unix.stackexchange.com/questions/107315/less-quit-if-one-screen-without-no-init.

--- zshenv.d/less.zsh
+++ zshenv.d/less.zsh
@@ -1,1 +1,1 @@
-export LESS="--hilite-search --hilite-unread --ignore-case --LONG-PROMPT --RAW-CONTROL-CHARS --chop-long-lines  --window=-4"
+export LESS="--quit-if-one-screen --hilite-search --hilite-unread --ignore-case --LONG-PROMPT --RAW-CONTROL-CHARS --chop-long-lines  --window=-4"
