3e36442a53f883bdf43a4cf363125ac5e0b6b5b7
James Harris <mailjamesharris@gmail.com>
2021-08-10 12:38:25 +1000

    Update macOS settings for Big Sur.

--- install.d/99-macos.bash
+++ install.d/99-macos.bash
@@ -1,56 +1,11 @@
 echo "Applying macOS defaults..."
 
-# Set a key delay (initial) and repeat to the fastest settings available from
-# the UI.
-defaults write NSGlobalDomain InitialKeyRepeat -int 15
-defaults write NSGlobalDomain KeyRepeat -int 2
+### DISPLAY ####################################################################
 
-# Enable full keyboard access for all controls
-# (e.g. enable Tab in modal dialogs)
-defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
-
-# Use function keys as actual function keys
-defaults write NSGlobalDomain com.apple.keyboard.fnState -bool true
-
-# Disable press-and-hold for keys in favor of key repeat
-defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
-
-# Disable janky animation when zooming windows (works intermittently)
-defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
-
-# Trackpad: enable tap to click for this user and for the login screen
-defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
-defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
-defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
-
-# Enable volume change feedback beep
-defaults write NSGlobalDomain com.apple.sound.beep.feedback -int 1
-
-# Disable UI sound effects (e.g. when emptying the bin)
-defaults write com.apple.systemsound com.apple.sound.uiaudio.enabled -int 0
-
-# Disable auto correct and other substitutions
-defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
-defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
-defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
-defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
-defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
-
-# Disable sound effects in Message.app.
-defaults write com.apple.messageshelper.AlertsController PlaySoundsKey -int 0
-
-# Disable auto correct and other substitutions in Message.app.
-defaults write com.apple.messageshelper.MessageController SOInputLineSettings -dict-add "automaticEmojiSubstitutionEnablediMessage" -bool false
-defaults write com.apple.messageshelper.MessageController SOInputLineSettings -dict-add "automaticQuoteSubstitutionEnabled" -bool false
-defaults write com.apple.messageshelper.MessageController SOInputLineSettings -dict-add "continuousSpellCheckingEnabled" -bool false
-
-# Set menu bar items
-defaults write com.apple.systemuiserver menuExtras -array \
-  "/System/Library/CoreServices/Menu Extras/Battery.menu" \
-  "/System/Library/CoreServices/Menu Extras/Clock.menu" \
-  "/System/Library/CoreServices/Menu Extras/Displays.menu" \
-  "/System/Library/CoreServices/Menu Extras/Volume.menu"
+# Turn off font smoothing on retina displays
+if system_profiler SPDisplaysDataType | grep 'retina' > /dev/null; then
+  defaults -currentHost write NSGlobalDomain AppleFontSmoothing -int 0
+fi
 
 # Date formats
 defaults write com.apple.menuextra.clock DateFormat -string "EEE MMM d h:mm a"
@@ -60,20 +15,92 @@ defaults write NSGlobalDomain AppleICUDateFormatStrings -dict \
   3 -string "y-MM-dd" \
   4 -string "y-MM-dd"
 
-# Hide Finder recent tags
-defaults write com.apple.finder ShowRecentTags -bool false
+# Disable janky animation when zooming windows (works intermittently)
+defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
 
-# Use list view in all Finder windows by default
+
+### KEYBOARD ###################################################################
+
+# Disable press-and-hold for keys in favor of key repeat
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+
+# Set a short initial delay and fast repeat rate
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
+defaults write NSGlobalDomain KeyRepeat -int 2
+
+# Enable full keyboard access for all controls (tab through inputs, etc)
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+
+# Use function keys as actual function keys
+defaults write NSGlobalDomain com.apple.keyboard.fnState -bool true
+
+# Disable auto correct and other substitutions
+defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+
+# Disable auto correct and other substitutions in Message.app.
+defaults write com.apple.messageshelper.MessageController SOInputLineSettings -dict-add "automaticEmojiSubstitutionEnablediMessage" -bool false
+defaults write com.apple.messageshelper.MessageController SOInputLineSettings -dict-add "automaticQuoteSubstitutionEnabled" -bool false
+defaults write com.apple.messageshelper.MessageController SOInputLineSettings -dict-add "continuousSpellCheckingEnabled" -bool false
+
+
+### MOUSE & TRACKPAD ###########################################################
+
+# Enable tap to click for this user and for the login screen
+defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+# Top-right hot corner + cmd turns on screen saver
+defaults write com.apple.dock wvous-tr-corner -int 5 # start screen saver
+defaults write com.apple.dock wvous-tr-modifier -int 1048576 # cmd key
+
+# Bottom-right hot corner + cmd disables screen saver
+defaults write com.apple.dock wvous-br-corner -int 6 # disable screen saver
+defaults write com.apple.dock wvous-br-modifier -int 1048576 # cmd key
+
+
+### SOUND ######################################################################
+
+# Disable UI sound effects (e.g. when emptying the bin)
+defaults write com.apple.systemsound com.apple.sound.uiaudio.enabled -int 0
+
+# Enable volume change feedback beep
+defaults write NSGlobalDomain com.apple.sound.beep.feedback -int 1
+
+# Disable sound effects in Message.app.
+defaults write com.apple.messageshelper.AlertsController PlaySoundsKey -int 0
+
+
+
+
+
+### DESKTOP & FINDER ###########################################################
+
+# Use list view by default
 defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
 
-# Keep directories at the top when sorting in Finder
+# List directories above files
 defaults write com.apple.finder _FXSortFoldersFirst -bool true
+
+# Hide recent tags
+defaults write com.apple.finder ShowRecentTags -bool false
+
+# Show the path bar
+defaults write com.apple.finder ShowPathbar -bool true
+
+# Hide the status bar
+defaults write com.apple.finder ShowStatusBar -bool false
 
 # Use the home directory as the default location for new Finder windows
 defaults write com.apple.finder NewWindowTarget -string "PfHm"
 defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/"
 
-# Finder should search the current directory by default
+# Search the current directory by default
 defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
 
 # Turn off automatic Desktop icons
@@ -82,8 +109,17 @@ defaults write com.apple.finder ShowHardDrivesOnDesktop -bool false
 defaults write com.apple.finder ShowMountedServersOnDesktop -bool false
 defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool false
 
+# Allow Finder to be quit normally
+defaults write com.apple.finder QuitMenuItem -bool true
+
+
+### DOCK, SPOTLIGHT, ETC #######################################################
+
 # Automatically hide and show the Dock
 defaults write com.apple.dock autohide -bool true
+
+# "Disable" the Dock by using a very long autohide delay
+defaults write com.apple.dock autohide-delay -float 1000
 
 # Show only open applications in the Dock
 defaults write com.apple.dock static-only -bool true
@@ -99,22 +135,34 @@ defaults write com.apple.dock magnification -bool true
 # Speed up Mission Control animations
 defaults write com.apple.dock expose-animation-duration -float 0.1
 
-# Disable Dashboard
-defaults write com.apple.dashboard mcx-disabled -bool true
-
 # Don’t show Dashboard as a Space
 defaults write com.apple.dock dashboard-in-overlay -bool true
 
 # Don’t automatically rearrange Spaces based on most recent use
 defaults write com.apple.dock mru-spaces -bool false
 
-# Top-right hot corner + cmd turns on screen saver
-defaults write com.apple.dock wvous-tr-corner -int 5 # start screen saver
-defaults write com.apple.dock wvous-tr-modifier -int 1048576 # cmd key
+# Disable Dashboard
+defaults write com.apple.dashboard mcx-disabled -bool true
 
-# Bottom-right hot corner + cmd disables screen saver
-defaults write com.apple.dock wvous-br-corner -int 6 # disable screen saver
-defaults write com.apple.dock wvous-br-modifier -int 1048576 # cmd key
+# Don't display first-time Spotlight messages
+defaults write com.apple.spotlight showedFTE 1
+defaults write com.apple.spotlight showedLearnMore 1
+
+
+### MAIL #######################################################################
+
+# Copy email addresses as `foo@example.com` instead of `Foo Bar <foo@example.com>`
+defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
+
+# Disable send and reply animations in Mail.app
+defaults write com.apple.mail DisableReplyAnimations -bool true
+defaults write com.apple.mail DisableSendAnimations -bool true
+
+# Disable inline attachments (just show the icons)
+defaults write com.apple.mail DisableInlineAttachmentViewing -bool true
+
+
+### OTHER ######################################################################
 
 # Require password immediately after sleep or screen saver begins
 defaults write com.apple.screensaver askForPassword -int 1
@@ -127,44 +175,6 @@ sudo defaults write /Library/Preferences/com.apple.SoftwareUpdate.plist Automati
 sudo defaults write /Library/Preferences/com.apple.SoftwareUpdate.plist ConfigDataInstall -bool true
 sudo defaults write /Library/Preferences/com.apple.SoftwareUpdate.plist CriticalUpdateInstall -bool true
 sudo defaults write /Library/Preferences/com.apple.commerce.plist AutoUpdate -bool true
-
-# Don't display first-time Spotlight messages
-defaults write com.apple.spotlight showedFTE 1
-defaults write com.apple.spotlight showedLearnMore 1
-
-# Copy email addresses as `foo@example.com` instead of `Foo Bar <foo@example.com>` in Mail.app
-defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
-
-# # Disable send and reply animations in Mail.app
-defaults write com.apple.mail DisableReplyAnimations -bool true
-defaults write com.apple.mail DisableSendAnimations -bool true
-
-# # Disable inline attachments (just show the icons)
-defaults write com.apple.mail DisableInlineAttachmentViewing -bool true
-
-# # Spotlight autocomplete sources
-defaults write com.apple.spotlight orderedItems -array \
-  '{"enabled" = 1;"name" = "APPLICATIONS";}' \
-  '{"enabled" = 1;"name" = "MENU_SPOTLIGHT_SUGGESTIONS";}' \
-  '{"enabled" = 1;"name" = "MENU_CONVERSION";}' \
-  '{"enabled" = 1;"name" = "MENU_EXPRESSION";}' \
-  '{"enabled" = 1;"name" = "MENU_DEFINITION";}' \
-  '{"enabled" = 1;"name" = "SYSTEM_PREFS";}' \
-  '{"enabled" = 0;"name" = "MESSAGES";}' \
-  '{"enabled" = 0;"name" = "DOCUMENTS";}' \
-  '{"enabled" = 0;"name" = "DIRECTORIES";}' \
-  '{"enabled" = 0;"name" = "PRESENTATIONS";}' \
-  '{"enabled" = 0;"name" = "SPREADSHEETS";}' \
-  '{"enabled" = 0;"name" = "PDF";}' \
-  '{"enabled" = 0;"name" = "MESSAGES";}' \
-  '{"enabled" = 0;"name" = "CONTACT";}' \
-  '{"enabled" = 0;"name" = "EVENT_TODO";}' \
-  '{"enabled" = 0;"name" = "IMAGES";}' \
-  '{"enabled" = 0;"name" = "BOOKMARKS";}' \
-  '{"enabled" = 0;"name" = "MUSIC";}' \
-  '{"enabled" = 0;"name" = "MOVIES";}' \
-  '{"enabled" = 0;"name" = "FONTS";}' \
-  '{"enabled" = 0;"name" = "MENU_OTHER";}'
 
 # Save screenshots to /tmp
 defaults write com.apple.screencapture location -string "/tmp"
