9e2cf0f5127c1da15f035c5ce20fd4965d86e73a
James Harris <contact@jamesharris.id.au>
2021-12-06 16:47:05 +1000

    Install git, go and GNU less.

--- zshenv.d/less.zsh
+++ zshenv.d/less.zsh
@@ -1,1 +1,4 @@
+# Note: This requires the GNU version of less to be installed via brew,
+# otherwise --quit-if-one-screen causes the Apple version of less to exit
+# without showing anything at all.
 export LESS="--quit-if-one-screen --hilite-search --hilite-unread --ignore-case --LONG-PROMPT --RAW-CONTROL-CHARS --chop-long-lines  --window=-4"
