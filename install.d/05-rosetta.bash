f53e662a44cc542fac7195abe332db4698bf9ab1
James Harris <mailjamesharris@gmail.com>
2021-12-06 14:59:09 +1000

    Automatically agree to Rosetta license.

--- install.d/05-rosetta.bash
+++ install.d/05-rosetta.bash
@@ -1,6 +1,6 @@
 if [[ "$(uname -p)" == "arm" ]]; then
      if [[ ! -f /Library/Apple/usr/share/rosetta/rosetta ]]; then
          echo "Installing Rosetta..."
-         sudo softwareupdate --install-rosetta
+         softwareupdate --install-rosetta --agree-to-license
      fi
  fi
