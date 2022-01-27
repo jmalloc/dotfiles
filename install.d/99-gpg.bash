6e1b5ac9bbaf1b91beb99dfb92051dbe434232d8
James Harris <contact@jamesharris.id.au>
2022-01-27 14:38:17 +1000

    Sign Git commits with PGP key.

---
+++ install.d/99-gpg.bash
@@ -0,0 +1,6 @@
+echo "Fetching private GPG key from 1Password..."
+
+gpg --batch --import <(op get document --account="$OP_ACCOUNT" "$OP_ITEM_GPGKEY")
+
+echo "default-cache-ttl 28800" > "$HOME/.gnupg/gpg-agent.conf"
+echo "pinentry-program /usr/local/bin/pinentry-mac" >> "$HOME/.gnupg/gpg-agent.conf"
