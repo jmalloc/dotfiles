1e0d5975d2dffd20f4e02f3301f5df106e5768c2
James Harris <contact@jamesharris.id.au>
2022-02-21 09:43:16 +1000

    Setup completion after brew but before grit.

--- zshrc.d/20-completion.zsh
+++ zshrc.d/20-completion.zsh
@@ -1,7 +1,8 @@
 # Initialize zsh auto-completion, and bash compatibility.
 #
-# This is done after everything else so that the other scripts have the
-# opportunity to modify FPATH.
+# This is done after _brew_ so that it has an opportunity to add itself to
+# FPATH, but before other commands (like Grit) that install Bash
+# autocompletions.
 autoload -U compinit
 autoload -U bashcompinit
 compinit
