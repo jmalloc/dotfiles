49e6cc5ab67b635914d78c3fdd476d28ba5f7a8b
James Harris <contact@jamesharris.id.au>
2025-01-02 13:36:20 +1000

    Set `LESS` and `EDITOR`.

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -100,6 +100,8 @@
 
       programs.zsh = {
         variables = {
+          EDITOR = "code";
+          LESS = "--quit-if-one-screen --hilite-search --hilite-unread --ignore-case --LONG-PROMPT --RAW-CONTROL-CHARS --chop-long-lines --window=-4";
           SSH_AUTH_SOCK = "$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
         };
       };
--- pre-nix/zshenv.d/50-editor.zsh
+++
@@ -1,7 +0,0 @@
-if hash code 2>/dev/null; then
-    export EDITOR="code"
-else
-    export EDITOR="vim"
-fi
-
-export VISUAL="$EDITOR"
