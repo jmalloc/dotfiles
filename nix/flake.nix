580771028b69e523c940e3aa6369df7b31365cee
James Harris <contact@jamesharris.id.au>
2025-01-06 09:04:13 +1000

    Configure 1password identity agent on a per-user basis.

--- nix/flake.nix
+++ nix/flake.nix
@@ -103,11 +103,6 @@
       };
 
       programs.ssh = {
-        extraConfig = ''
-          Host *
-            IdentityAgent "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
-        '';
-
         knownHosts = {
           github_ed25519 = {
             hostNames = ["github.com"];
