a2e0dff7a7f47388d8d5b49c9686730df58542d3
James Harris <contact@jamesharris.id.au>
2025-01-07 12:11:10 +1000

    Split macOS defaults into separate files.

--- nix/flake.nix
+++ nix/flake.nix
@@ -54,72 +54,8 @@
           };
         };
 
-        power = {
-          restartAfterPowerFailure = true;
-          restartAfterFreeze = true;
-
-          sleep = {
-            computer = "never";
-            display = "never";
-          };
-        };
-
-        system.defaults = {
-          NSGlobalDomain = {
-            AppleKeyboardUIMode = 3;
-            "com.apple.keyboard.fnState" = true;
-
-            ApplePressAndHoldEnabled = false;
-            InitialKeyRepeat = 15;
-            KeyRepeat = 2;
-
-            NSAutomaticCapitalizationEnabled = false;
-            NSAutomaticDashSubstitutionEnabled = false;
-            NSAutomaticPeriodSubstitutionEnabled = false;
-            NSAutomaticQuoteSubstitutionEnabled = false;
-            NSAutomaticSpellingCorrectionEnabled = false;
-
-            "com.apple.mouse.tapBehavior" = 1;
-            "com.apple.sound.beep.feedback" = 1; # beep when adjusting volume
-          };
-
-          controlcenter = {
-            Sound = true; # show sound icon on menubar
-          };
-
-          screensaver = {
-            askForPassword = true;
-            askForPasswordDelay = 0;
-          };
-
-          dock = {
-            static-only = true;
-            persistent-apps = [ ];
-            autohide = true;
-            autohide-delay = 1000.0; # "disable" the dock
-          };
-
-          finder = {
-            FXRemoveOldTrashItems = true;
-          };
-
-          screencapture = {
-            location = "/tmp";
-            type = "png";
-            show-thumbnail = false;
-          };
-
-          CustomUserPreferences = {
-            NSGlobalDomain = {
-              "com.apple.sound.uiaudio.enabled" = 0; # disable UI sound effects
-            };
-
-            "com.apple.dock" = {
-              wvous-tr-corner = 5; # top-right hot-corner = screensaver
-              wvous-tr-modifier = 1048576; # command key
-            };
-          };
-        };
+        power = import ./macos.power.nix;
+        system.defaults = import ./macos.defaults.nix;
 
         # Necessary for using flakes on this system.
         nix.settings.experimental-features = "nix-command flakes";
---
+++ nix/macos.power.nix
@@ -0,0 +1,9 @@
+{
+    restartAfterPowerFailure = true;
+    restartAfterFreeze = true;
+
+    sleep = {
+      computer = "never";
+      display = "never";
+    };
+}
