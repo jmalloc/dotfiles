f73883e27faf43ef62bb1683fb93dde0faa3d863
James Harris <contact@jamesharris.id.au>
2025-01-10 14:15:31 +1000

    Prepend `$HOME/bin` (though it is still after homebrew paths :/)

--- home.nix
+++ home.nix
@@ -1,34 +1,40 @@
 { pkgs, ... }: {
-  # These packages do not require any additional configuration.
-  #
-  # If a package _does_ require further configuration, or has related shell
-  # variables, aliases, etc, then move it to a separate module and add it to the
-  # import list below.
-  home.packages = [
-    pkgs.evans
-    pkgs.gh
-    pkgs.google-chrome
-    pkgs.graphviz
-    pkgs.jq
-    pkgs.kcl
-    pkgs.mob
-    pkgs.nixpkgs-fmt
-    pkgs.nodejs
-    pkgs.pgcli
-    pkgs.unixtools.watch
-    pkgs.vale
-    pkgs.wabt
-    pkgs.wasmer
-    pkgs.yarn
-    pkgs.yq-go
-    pkgs.zoom-us
-  ];
+  home = {
+    # These packages do not require any additional configuration.
+    #
+    # If a package _does_ require further configuration, or has related shell
+    # variables, aliases, etc, then move it to a separate module and add it to the
+    # import list below.
+    packages = [
+      pkgs.evans
+      pkgs.gh
+      pkgs.google-chrome
+      pkgs.graphviz
+      pkgs.jq
+      pkgs.kcl
+      pkgs.mob
+      pkgs.nixpkgs-fmt
+      pkgs.nodejs
+      pkgs.pgcli
+      pkgs.unixtools.watch
+      pkgs.vale
+      pkgs.wabt
+      pkgs.wasmer
+      pkgs.yarn
+      pkgs.yq-go
+      pkgs.zoom-us
+    ];
 
-  # Aliases for things that are installed via Brew.
-  home.shellAliases = {
-    b = "bsctl";
-    d = "docker";
-    tp = "telepresence";
+    sessionVariables = {
+      PATH = "$HOME/bin:$PATH";
+    };
+
+    # Aliases for things that are installed via Brew.
+    shellAliases = {
+      b = "bsctl";
+      d = "docker";
+      tp = "telepresence";
+    };
   };
 
   imports = [
