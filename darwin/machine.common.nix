7200ac0bfcce22df4cb42e1c2428777bf4a8091c
James Harris <contact@jamesharris.id.au>
2025-07-30 17:29:03 +1000

    Only chmod sudoers file if it exists.

--- darwin/machine.common.nix
+++ darwin/machine.common.nix
@@ -67,7 +67,7 @@
   };
 
   system.activationScripts.extraActivation.text = lib.mkAfter ''
-    chmod 0440 /etc/sudoers.d/10-nix-darwin-extra-config
+    test -f /etc/sudoers.d/10-nix-darwin-extra-config && chmod 0440 /etc/sudoers.d/10-nix-darwin-extra-config
     sudo visudo --check --strict
   '';
 }
